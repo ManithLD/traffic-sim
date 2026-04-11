24bedcc4838ab594e9138a64f56bfc9dce4776ea
ManithLD <manithlakvindu.d@gmail.com>
2026-04-11 16:10:17 -0400

    Add sync colour changes to traffic light points

--- src/components/RoadNetwork.tsx
+++ src/components/RoadNetwork.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Polyline, CircleMarker } from 'react-leaflet'
 import { RoadNetwork } from '../simulation/mapLoader'
+
+const SIGNAL_COLORS = ['red', 'yellow', 'green'];
 
 interface Props {
   network: RoadNetwork
 }
 
 export default function RoadNetworkLayer({ network }: Props) {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  useEffect(() => {
+      const timer = setInterval(() => {
+        setColorIndex((prevIndex) => (prevIndex + 1) % SIGNAL_COLORS.length);
+      }, 3000);
+
+      return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
       {network.roads.map((road) => (
@@ -20,11 +32,11 @@ export default function RoadNetworkLayer({ network }: Props) {
       ))}
       {network.signals.map((signal) => (
         <CircleMarker
-          key={signal.id}
+          key={`${signal.id}-${colorIndex}`}
           center={[signal.lat, signal.lon]}
           radius={5}
-          color="red"
-          fillColor="red"
+          color={SIGNAL_COLORS[colorIndex]}
+          fillColor={SIGNAL_COLORS[colorIndex]}
           fillOpacity={1}
         />
       ))}
